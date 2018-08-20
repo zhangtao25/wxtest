f8fa4baf639cc3daa8db3013f104715fd459db04
张张涛 <zhangzhangtao@zhangzhtaodeMBP.lan>
2018-08-21 00:46:39 +0800

    8/21 12:46

--- pages/home-components/find-us/index.js
+++ pages/home-components/find-us/index.js
@@ -7,6 +7,13 @@ Component({
       longitude: 118.524110,
       height: 50
     }],
+    markers1: [{
+      iconPath: "/images/static/map-icon.png",
+      id: 1,
+      latitude: 31.639443,
+      longitude: 118.497606,
+      height: 50
+    }],
     polyline: [{
       points: [{
         longitude: 113.3245211,
@@ -18,7 +25,9 @@ Component({
       color:"#FF0000DD",
       width: 2,
       dottedLine: true
-    }]
+    }],
+    navbar: ['北店', '南店'],                              //记录不同状态记录的数量
+    currentTab: 0,
   },
   methods: {
     getDate(){
@@ -40,6 +49,26 @@ Component({
             address:'马鞍山耀华汽车配件有限公司'
           })
         }
+      })
+    },
+    openMap1(){
+      wx.getLocation({//获取当前经纬度
+        type: 'wgs84', //返回可以用于wx.openLocation的经纬度，官方提示bug: iOS 6.3.30 type 参数不生效，只会返回 wgs84 类型的坐标信息  
+        success: function (res) {
+          wx.openLocation({//​使用微信内置地图查看位置。
+            latitude: 31.639443,//要去的纬度-地址
+            longitude: 118.497606,//要去的经度-地址
+            name: "福耀汽车玻璃(马鞍山耀华店)",
+            address:'福耀汽车玻璃(马鞍山耀华店)'
+          })
+        }
+      })
+    },
+    //响应点击导航栏
+    navbarTap: function (e) {
+      var that = this;
+      that.setData({
+        currentTab: e.currentTarget.dataset.idx
       })
     }
   }
